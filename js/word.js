358a5690fbb79f0234e4673aef635f08fbda79e5
preshengr <preshengr@mail.com>
2024-09-09 11:47:20 +0100

    js directory updated

---
+++ js/word.js
@@ -0,0 +1,198 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "An Object Oriented Programming Language"
+    },
+    {
+        word: "guitar",
+        hint: "A Six String Musical Instrument on a long word"
+    },
+    {
+        word: "aim",
+        hint: "To have a Focus"
+    },
+    {
+        word: "venus",
+        hint: "The Second Planet in the Solar System"
+    },
+    {
+        word: "gold",
+        hint: "A Yellow Like Precious Stone"
+    },
+    {
+        word: "keyboard",
+        hint: "A musical Board with White and Black Keys"
+    },
+    {
+        word: "golang",
+        hint: "A statically typed, compiled high-level programming language designed at Google"
+    },
+    {
+        word: "coding",
+        hint: "The act of programming is called"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie with a character called NEO"
+    },
+    {
+        word: "bugs",
+        hint: "an error that makes a code fail"
+    },
+    {
+        word: "computer",
+        hint: "its a garbage in, garbage out device"
+    },
+    {
+        word: "png",
+        hint: "a file format for image with transparent background"
+    },
+    {
+        word: "Sun",
+        hint: "Produces heat in the day and close to mecury"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "volley",
+        hint: "a famous indoor game played over a net"
+    },
+    {
+        word: "chess",
+        hint: "related to a indoor game and played on the board"
+    },
+    {
+        word: "viber",
+        hint: "a social media app fro chatting"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "gif",
+        hint: "an image file format for sliding images"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device that allows for calls"
+    },
+    {
+        word: "cpu",
+        hint: "the Central processing system"
+    },
+    {
+        word: "java",
+        hint: "programming language that works with html and css close to Java"
+    },
+    {
+        word: "google",
+        hint: "a famous search engine thats owns android"
+    },
+    {
+        word: "venice",
+        hint: "the famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows for spreadsheet activities"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "Africa",
+        hint: "a continent of blacks"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+]
